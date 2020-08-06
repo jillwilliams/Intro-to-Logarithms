5c0a8b424736540e216692bdf059968177ab43ac
jillwilliams <codeGeek.03.14@gmail.com>
2020-08-05 19:51:04 -0700

    Final Draft

---
+++ script.js
@@ -0,0 +1,28 @@
+const myProblem = () => {
+    var answer = 134.56;
+    return answer;
+};
+const myCalc = () => {
+    document.getElementById("demo").innerHTML = myProblem();
+}
+const myProblem2 = () => {
+    var answer = 1928.23;
+    return answer;
+};
+const myCalc2 = () => {
+    document.getElementById("demo1").innerHTML = myProblem2();
+}
+const myProblem3 = () => {
+    var answer = 469.76;
+    return answer;
+};
+const myCalc3 = () => {
+    document.getElementById("demo2").innerHTML = myProblem3();
+}
+const myProblem4 = () => {
+    var answer = 14263.1;
+    return answer;
+};
+const myCalc4 = () => {
+    document.getElementById("demo3").innerHTML = myProblem4();
+}
